33f495a4a9eca7652f9071a80b4e08627e079806
Ashish Ekka <ashishekka329@gmail.com>
2019-03-27 13:54:20 +0530

    Updates

---
+++ precache-manifest.d38d40c6bdb11f4cf04318376eed4692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d81ce867cd4dadf6de5d",
+    "url": "/random-quote-generator/static/js/runtime~main.df6529ef.js"
+  },
+  {
+    "revision": "82ac8198a2718d8b4ed1",
+    "url": "/random-quote-generator/static/js/main.8f1c7066.chunk.js"
+  },
+  {
+    "revision": "1a7fb439f5899dec0424",
+    "url": "/random-quote-generator/static/js/2.6e7c31bc.chunk.js"
+  },
+  {
+    "revision": "82ac8198a2718d8b4ed1",
+    "url": "/random-quote-generator/static/css/main.32cdb845.chunk.css"
+  },
+  {
+    "revision": "1a7fb439f5899dec0424",
+    "url": "/random-quote-generator/static/css/2.0c6bfe96.chunk.css"
+  },
+  {
+    "revision": "54576167a247bb42636c9908b4931209",
+    "url": "/random-quote-generator/index.html"
+  }
+];
